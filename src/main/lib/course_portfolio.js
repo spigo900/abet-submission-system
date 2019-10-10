dc24ae238486affe9db828fd93f7161b5f5dd252
Joseph Cecil <cecil@ads.isi.edu>
2019-10-10 19:04:47 -0400

    Make course_portfolio work right.

--- src/main/lib/course_portfolio.js
+++ src/main/lib/course_portfolio.js
@@ -1,7 +1,14 @@
 const Portfolio = require('../models/CoursePortfolio')
 const Course = require('../models/Course')
-const PortfolioSLO = require('../models/CoursePortfolio/StudentLearningOutcome')
+const StudentLearningOutcome = require('../models/StudentLearningOutcome')
 const { transaction } = require('objection')
+
+module.exports.BadCourseError = function(message, extra) {
+	Error.captureStackTrace(this, this.constructor)
+	this.name = this.constructor.name
+	this.message = message
+	this.extra = extra
+}
 
 module.exports.new = async ({
 	department_id,
@@ -17,31 +24,53 @@ module.exports.new = async ({
 	let course = await Course.query()
 		.where('department_id', department_id)
 		.where('number', course_number)
-		.first();
+		.first()
 	// TODO: better way?
 	if (!course) {
-		throw Error(`you entered a bad course! dept_id=${department_id}, course_number=${course_number} is not a real course!`)
+		throw new BadCourseError(
+			`you entered a bad course! dept_id=${department_id}, `
+			`course_number=${course_number} is not a real course!`,
+			{department_id, course_number}
+		)
 	}
 
-	// TODO: do this as a transaction (I don't know how)
-	const new_portfolio = await Portfolio.query()
-		.debug('enabled')
-		.insert({
-			course_id: parseInt(course.id),
-			instructor_id: parseInt(instructor),
-			semester_term_id: parseInt(semester),
-			num_students: parseInt(num_students),
-			section: parseInt(section),
-			year: parseInt(year)
-		});
-	
-	// add SLOs
-	for (slo_id of student_learning_outcomes) {
-		await new_portfolio.$relatedQuery('outcomes')
+	// We want adding a portfolio and its SLOs to be atomic. If there's an
+	// error partway through, we don't want to commit a partial course
+	// portfolio to the database. For example, if one of the SLO indices
+	// passed in doesn't refer to a real SLO, we don't want to commit a
+	// partial portfolio.
+	// TODO: I could do the "find the correct SLOs" logic cleaner.
+	let trx
+	let new_portfolio
+	try {
+		trx = await transaction.start(Portfolio.knex())
+		new_portfolio = await Portfolio.query(trx)
 			.insert({
-				portfolio_id: new_portfolio.id,
-				slo_id: parseInt(slo_id)
-			});
+				course_id: parseInt(course.id),
+				instructor_id: parseInt(instructor),
+				semester_term_id: parseInt(semester),
+				num_students: parseInt(num_students),
+				section: parseInt(section),
+				year: parseInt(year)
+			})
+
+		// Add SLOs to the database.
+		for (slo_index of student_learning_outcomes) {
+			let slo = await StudentLearningOutcome.query()
+				.where('index', parseInt(slo_index))
+				.first()
+			
+			await new_portfolio.$relatedQuery('outcomes', trx)
+				.insert({
+					portfolio_id: new_portfolio.id,
+					slo_id: slo.id
+				})
+		}
+
+		trx.commit()
+	} catch (err) {
+		// TODO: Do more than just roll this back! Raise the error or something so that it's not a silent error! Silent errors are death!
+		trx.rollback()
 	}
 
 	return new_portfolio;
